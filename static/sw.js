68054e785b36b9e9db394f12f479ebf083d86e32
RJ Paul Tabarno <rjpaulhemodo@gmail.com>
2026-04-29 20:07:09 +0800

    Wipe repository for complete GoRoute UI redesign

--- static/sw.js
+++
@@ -1,8 +0,0 @@
-// A simple service worker to allow PWA installation
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Allows the app to work offline by caching (we will leave this basic for now)
-});
